5a36b54fc21c59ad704b723930b95beb7f0c418f
littleFlowerPot <przet@tpg.com.au>
2016-08-03 20:39:27 +0930

    Add files via upload

---
+++ LippmanEx242_227b.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main ()
+{
+cout<<"Lippman Exercises242, exercise 227b \n\n";
+
+cout<<"Is int *const p2 = &i2 legal? \n\n";
+
+cout<<"First, the syntax makes sense, so that checks out. Now lets analyse what the expression means."
+<<"The expression translates to:\n\n";
+
+cout<<"p2 is a constant pointer to i2, which is of type int
+
+
+  return 0;
+}
