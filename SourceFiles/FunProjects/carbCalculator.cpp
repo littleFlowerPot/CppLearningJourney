f1977fb6a92bb2fb020fbc3ea7412996633765c5
someone <someone@someplace.com>
2016-10-05 19:35:55 +1030

    continued working on my carb calculator

--- SourceFiles/FunProjects/carbCalculator.cpp
+++ SourceFiles/FunProjects/carbCalculator.cpp
@@ -17,13 +17,44 @@ cout<<'\n';
 //TODO:create strings and database storage where I can: list the item I  have consumed, and then enter into a database and then query that database for grams of carb.
 
 //Some CLI output for the user.
-cout<<"Please enter the current list of grams of carbs consumed (or your previous total), each entry space seperated. Please press return and then ctrl+d (linux) or ctrl+z (windows) when done.";
+cout<<"Please enter the amount of carb sources consumed:";
 cout<<'\n';
 cout<<'\n';
+double amountOfMilk = 0.0;
+double milkCarbsFactor=4.7/100;
 
-//intialise an integer variable, and a sum variable summation holder, and then create a while loop to accept many inputs.
-int v=0; //good practice to reduce compile errors: initialise!
-int sum=0;
+double amountOfChocPowder = 0.0;
+double chocPowderCarbsFactor = 0.66;
+
+double amountOfSugar = 0.0;
+
+double amountOfCornFlakes = 0.0;
+double cornFlakesCarbsFactor = 24/30;
+
+double amountOfPasta = 0.0;
+double pastaCarbsFactor = 71.2/100;
+
+cin>> amountOfMilk, amountOfChocPowder, amountOfSugar, amountOfCornFlakes, amountOfPasta;
+double amountOfCarbsFromMilk= milkCarbsFactor*amountOfMilk;
+double amountOfCarbsFromChocPowder=chocPowderCarbsFactor*amountOfChocPowder;
+double amountOfCarbsFromSugar = amountOfSugar; //sugar is carbs
+double amountOfCarbsFromCornFlakes=amountOfCornFlakes*cornFlakesCarbsFactor;
+double amountOfCarbsFromPasta=amountOfPasta*pastaCarbsFactor;
+
+double sum = amountOfCarbsFromMilk+amountOfCarbsFromChocPowder+amountOfCarbsFromSugar+amountOfCarbsFromCornFlakes+amountOfCarbsFromPasta;
+
+cout<<"\n\n";
+cout<<"The number of carbs consumed is "<<sum<<"g";
+cout<<"\n\n"; 
+
+
+
+
+ 
+/*
+//intialise an double variable, and a sum variable summation holder, and then create a while loop to accept many inputs.
+double v=0; //good practice to reduce compile errors: initialise!
+double sum=0;
 	while (cin>>v)
 		sum = sum + v;
 
@@ -34,5 +65,6 @@ cout<<'\n';
 
 //TODO:Store the sum for use next time calculator is invoked.
 
+*/
 return 0;
 }
